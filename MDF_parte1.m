a8cd746f9a2042076def599cd191ab85b1eb3328
Gustavo S <gustavonnsanches@usp.br>
2025-05-21 21:32:09 -0300

    Tentando consertar...

--- MDF_parte1.m
+++ MDF_parte1.m
@@ -7,7 +7,7 @@ d = 5*h;
 L = 2*h;
 H = 8*h;
 
-dx = 1;
+dx = 2;
 dy = dx;
 
 x = 0:dx:(2*d+L);
@@ -70,7 +70,8 @@ grid on;
 % Resolucao da eq de Laplace
 phi = zeros(size(X));
 erro = 1;
-while max(abs(erro(:))) > toleracia
+cont = 0;
+while cont < 100 %max(abs(erro(:))) > toleracia
     phi = cond_contorno(x,y,H,V,dx,dy,phi);
     phi_aux = phi;
     for coluna= 2:length(x)/2+1 
@@ -82,6 +83,8 @@ while max(abs(erro(:))) > toleracia
     erro = phi-phi_aux;
     disp(max(abs(erro(:))));
     phi = phi_aux;
+    cont = cont+1;
+    mesh(phi);
 end
 
 %Condicoes de contorno
@@ -90,7 +93,7 @@ function phi = cond_contorno(x,y,H,V,dx,dy,phi)
 %Margem esquerda
     for j = 1:length(y)
         if j*dy <= 0.05*H
-            phi(j,1) = V*(dx^2)/(0.1*H) + phi(j,2);
+             phi(j,1) = V*(dx^2)/(0.1*H) + phi(j,2);
         else
             phi(j,1) = phi(j,2);
         end
@@ -112,26 +115,29 @@ function phi = cond_contorno(x,y,H,V,dx,dy,phi)
 end
 
 function val_phi_novo = passo(phi, lambda,linha,coluna,isSolido,d,dx,L,h,dy)
+    
+    %Define o valor no prédio.
     if isSolido(linha,coluna) 
         val_phi_novo = 0;  
-    elseif isSolido(linha,coluna+1) || isSolido(linha-1,coluna)
+
+    %Utiliza cond. de contorno irregulares se perto do prédio.
+    elseif isSolido(linha,coluna+1) || isSolido(linha-1,coluna) 
         a = 1;
         b = 1;
-        if (coluna-1)*dx < d
+        if (coluna-1)*dx < d %Só na parede lateral.
             a = (d-(coluna-1)*dx)/dx ;
-        else
-            a = (L/2 - sqrt( (L/2)^2 - ((linha-1)*dy - h)^2 ))/dy;
-            if a>dx
+        else %Nos contornos do teto circular
+            a = ((d+L/2-(coluna-1)*dx) - sqrt( (L/2)^2 - ((linha-1)*dy - h)^2 ))/dy;
+            if ~isSolido(linha,coluna+1)
                 a=1;
             end
             b = ((linha-1)*dy -h - sqrt((L/2)^2 - (d+(L/2) - (coluna-1)*dx)^2))/dy;
+            if ~isSolido(linha-1,coluna)
+                b=1;
+            end
         end
-         
+        %Calcula o valor usando as proporções.
         val_phi_novo = ((a*b*dx^2*dy^2)/(a*dx^2 + b*dy^2))*((phi(linha,coluna-1)/(dx^2*(a+1)))+(phi(linha+1,coluna)/dy^2*(b+1)));
-    %|| isSolido(x-1,y) || isSolido(x,y-1) || isSolido(x,y+1) %Verifica se está a esquerda do prédio.
-    %Calcular as proporções
-    %Resolver os sistemas
-    %Devolve o valor val_phi_novo = ...
     else
     %Executa sobrerrelaxação tradicional se não está perto do prédio.
     val_phi_novo = (1-lambda)*phi(linha,coluna) + (lambda/4)*(phi(linha-1,coluna)+phi(linha+1,coluna)+phi(linha,coluna-1)+phi(linha,coluna+1));
