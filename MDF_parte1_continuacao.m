8e0ebcee0647c9ce8e3063a35cf2464b0c15f3bd
Flavio Silva Ferro <flavioferro@usp.br>
2025-05-22 18:24:17 -0300

    FOI - parte flavio

--- MDF_parte1_continuacao.m
+++ MDF_parte1_continuacao.m
@@ -7,7 +7,7 @@ d = 5*h;
 L = 2*h;
 H = 8*h;
 
-dx = 0.33;
+dx = 1;
 dy = dx;
 
 x = 0:dx:(2*d+L);
@@ -69,76 +69,144 @@ xticks(min(x):dx:max(x));  % Linhas verticais a cada dx
 yticks(min(y):dy:max(y));  % Linhas horizontais a cada dy
 grid on;
 
-% Criação de matriz de testes
-
-% Inicializar psi com escoamento uniforme (V*y)
-psi = V * Y; % ψ = V*y (escoamento uniforme na direção x)
-
-% Ajustar condições de contorno:
-% 1. ψ = 0 na base (y = 0)
-psi(Y == 0) = 0;
-
-% 2. Dentro do sólido, ψ = constante (aqui, 0)
+psi = 500*ones(size(X));
 psi(isSolido) = 0;
+psi = cond_contorno(X,Y,dx,dy,H,V,psi);
 
-% 3. Condições laterais (opcional, se necessário):
-% - Na entrada (x = 0) e saída (x = 2d + L), manter ψ = V*y
-% - Se houver paredes laterais sólidas, ajustar ψ para constante
+erro = 1;
+while max(abs(erro(:))) > toleracia
+    psi_aux = psi;
+    for i=2:size(X,2)-1
+        for j=2:size(Y,1)-1
+            if ~isSolido(j,i)
+                if isSolido(j,i-1) || isSolido(j,i+1) || isSolido(j-1,i)
+                    if isSolido(j, i+1) && ~isSolido(j-1, i)
+                        %Quadrante 1
+                        a = (d-X(j,i))/dx;
+                        psi(j,i)=(2/(a*dx^2)+2/dy^2)^(-1)*(2*psi(j,i-1)/(dx^2*(a+1))+(psi(j+1,i)+psi(j-1,i))/dy^2);
+                    
+                    elseif isSolido(j, i+1) && isSolido(j-1, i)
+                        %Quadrante 2
+                        a = ((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx;
+                        b = (Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy;
+                        psi(j,i)=(2/(a*dx^2)+2/(b*dy^2))^(-1)*(2*psi(j,i-1)/(dx^2*(a+1))+2*psi(j+1,i)/(dy^2*(b+1))); 
+                    
+                    elseif isSolido(j, i-1) && isSolido(j-1, i)
+                        %Quadrante 3
+                        a = ((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx;
+                        b = (Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy;
+                        psi(j,i)=(2/(a*dx^2)+2/(b*dy^2))^(-1)*(2*psi(j,i+1)/(dx^2*(a+1))+2*psi(j+1,i)/(dy^2*(b+1)));
+                        
+                    else %isSolido(j, i-1) && ~isSolido(j-1, i)
+                        %Quadrante 4
+                        a = (X(j,i)-(d+L))/dx;
+                        psi(j,i)=(2/(a*dx^2)+2/dy^2)^(-1)*(2*psi(j,i+1)/(dx^2*(a+1))+(psi(j+1,i)+psi(j-1,i))/dy^2);
+                        
+                    end
+                else
+                    %Pontos interiores
+                    psi(j,i) = (psi(j,i+1)+psi(j,i-1)+psi(j+1,i)+psi(j-1,i))/4;
+                end    
+            end 
+        end
+    end
+    psi(2:end-1,2:end-1)=lambda*psi(2:end-1,2:end-1)+(1-lambda)*psi_aux(2:end-1,2:end-1);
+    psi = cond_contorno(X,Y,dx,dy,H,V,psi);
+    erro = abs((psi-psi_aux)./psi);
+    display(max(erro(:)));
+    contourf(X, Y, psi, 20, 'LineColor', 'none');
+    colorbar;
+    axis equal;
+    xlabel('x'); ylabel('y');
+    title('Função de Corrente Genérica (\psi)');
+end
 
-% Exibir a matriz psi - Item A
-figure;
-contourf(X, Y, psi, 20, 'LineColor', 'none');
-colorbar;
-axis equal;
-xlabel('x'); ylabel('y');
-title('Função de Corrente Genérica (\psi)');
-hold on;
-scatter(X(isSolido), Y(isSolido), 2, 'r', 'filled'); % Destacar o sólido
 
-% Calcular velocidades (apenas para pontos fora do sólido)
-u = zeros(size(psi));
-v = zeros(size(psi));
-u(~isSolido) = gradient(psi(~isSolido), dy);  % u = ∂ψ/∂y
-v(~isSolido) = -gradient(psi(~isSolido), dx); % v = -∂ψ/∂x
 
-% Plotar campo de velocidade (setas pretas) - ItemB
-figure;
-quiver(X(mask), Y(mask), u(mask), v(mask), ...
-    'AutoScaleFactor', 1.5, ...
-    'Color', 'k', ...          % Define cor preta
-    'LineWidth', 1.2);         % Espessura das setas
-axis equal;
-grid on;                       % Adiciona grade
-xlabel('x (m)'); 
-ylabel('y (m)');
-title('Campo de Velocidade (Genérico)');
+% % Exibir a matriz psi - Item A
+% figure;
+% contourf(X, Y, psi, 20, 'LineColor', 'none');
+% colorbar;
+% axis equal;
+% xlabel('x'); ylabel('y');
+% title('Função de Corrente Genérica (\psi)');
+% hold on;
+% scatter(X(isSolido), Y(isSolido), 2, 'r', 'filled'); % Destacar o sólido
+% 
+% % Calcular velocidades (apenas para pontos fora do sólido)
+% u = zeros(size(psi));
+% v = zeros(size(psi));
+% u(~isSolido) = gradient(psi(~isSolido), dy);  % u = ∂ψ/∂y
+% v(~isSolido) = -gradient(psi(~isSolido), dx); % v = -∂ψ/∂x
+% 
+% % Plotar campo de velocidade (setas pretas) - ItemB
+% figure;
+% quiver(X(mask), Y(mask), u(mask), v(mask), ...
+%     'AutoScaleFactor', 1.5, ...
+%     'Color', 'k', ...          % Define cor preta
+%     'LineWidth', 1.2);         % Espessura das setas
+% axis equal;
+% grid on;                       % Adiciona grade
+% xlabel('x (m)'); 
+% ylabel('y (m)');
+% title('Campo de Velocidade (Genérico)');
+% 
+% % Ajustar limites dos eixos (se necessário)
+% xlim([min(x), max(x)]);
+% ylim([min(y), max(y)]);
+% 
+% %Pressão item C
+% 
+% % Calcular a pressão (equação corrigida)
+% gamma_term = (gamma_ar - 1) / gamma_ar;
+% DeltaP = rho * gamma_term * ( (V^2)/2 - (u.^2 + v.^2)/2 );
+% 
+% % Mascarar pontos dentro do sólido (isSolido)
+% DeltaP(isSolido) = NaN; % Ignorar pressão no interior
+% 
+% % Plotar pressão
+% figure;
+% contourf(X, Y, DeltaP, 50, 'LineColor', 'none');
+% colorbar;
+% colormap('jet');
+% hold on;
+% scatter(X(isSolido), Y(isSolido), 2, 'k', 'filled'); % Destacar o sólido
+% axis equal;
+% xlabel('x (m)');
+% ylabel('y (m)');
+% title('Distribuição de Pressão Relativa (p - p_{atm}) [Pa]');
+% 
+% % Item D
 
-% Ajustar limites dos eixos (se necessário)
-xlim([min(x), max(x)]);
-ylim([min(y), max(y)]);
+function psi = cond_contorno(X,Y,dx,dy,H,V,psi)
+    
+    %Margem inferior
+    psi(1,:)=0;
+    
+    %Margem esquerda
+    for j = 2:size(Y,1)-1
+        if Y(j,1)<=0.05*H
+            psi(j,1)=psi(j,2)+V*dx^2/(0.1*H);
+        else
+            psi(j,1)=psi(j,2);
+        end
+    end
+    
+    %Margem direita
+    for j = 2:size(Y,1)-1
+        if Y(j,end)<=0.05*H
+            psi(j,end)=psi(j,end-1)+V*dx^2/(0.1*H);
+        else
+            psi(j,end)=psi(j,end-1);
+        end
+    end
+    
+    %Margem superior
+    for i = 1:size(X,2)
+        psi(end,i) = psi(end-1,i)+dy*V;
+    end
 
-%Pressão item C
-
-% Calcular a pressão (equação corrigida)
-gamma_term = (gamma_ar - 1) / gamma_ar;
-DeltaP = rho * gamma_term * ( (V^2)/2 - (u.^2 + v.^2)/2 );
-
-% Mascarar pontos dentro do sólido (isSolido)
-DeltaP(isSolido) = NaN; % Ignorar pressão no interior
-
-% Plotar pressão
-figure;
-contourf(X, Y, DeltaP, 50, 'LineColor', 'none');
-colorbar;
-colormap('jet');
-hold on;
-scatter(X(isSolido), Y(isSolido), 2, 'k', 'filled'); % Destacar o sólido
-axis equal;
-xlabel('x (m)');
-ylabel('y (m)');
-title('Distribuição de Pressão Relativa (p - p_{atm}) [Pa]');
-
-% Item D
+end
 
 
 
--- MDF_parte1_flavio.m
+++ MDF_parte1_flavio.m
@@ -17,7 +17,7 @@ y = 0:dy:H;
 
 % Outros parâmetros do problema
 
-V = 100*3.6; %Velocidade em m/s!
+V = 100/3.6; %Velocidade em m/s!
 lambda = 1.85;
 toleracia = 0.01;
 rho = 1.25;    % Densidade do ar (kg/m³)
@@ -35,7 +35,7 @@ for i = 1:length(x)
         if xi >= d && xi <= d + L
             y_teto = sqrt((L/2)^2 - (xi - (d + L/2))^2) + h;
             if yj <= y_teto
-                isSolido(j,i) = true; 
+                isSolido(j,i) = true;
             end
         end
     end
@@ -56,157 +56,143 @@ x_galpao = [x_parede_esq, x_telha_esq, x_telha, x_telha_dir, x_parede_dir];
 y_galpao = [y_parede_esq, y_telha_aux, y_telha, y_telha_aux, y_parede_dir];
 
 
-% Plota apenas os pontos da malha (isSolido == false)
+% % Plota apenas os pontos da malha (isSolido == false)
 mask = ~isSolido;  % true para pontos de malha válidos
-scatter( X(mask), Y(mask), 20, 'k', 'filled' )
-hold on
-scatter(x_galpao, y_galpao, 10, 'r', 'filled')
-axis equal
-xlabel('x'); ylabel('y');
-title('Pontos da malha (excluindo o interior do galpão)');
-% Configurar o grid com espaçamento dx e dy
-xticks(min(x):dx:max(x));  % Linhas verticais a cada dx
-yticks(min(y):dy:max(y));  % Linhas horizontais a cada dy
-grid on;
+% scatter( X(mask), Y(mask), 20, 'k', 'filled' )
+% hold on
+% scatter(x_galpao, y_galpao, 10, 'r', 'filled')
+% axis equal
+% xlabel('x'); ylabel('y');
+% title('Pontos da malha (excluindo o interior do galpão)');
+% % Configurar o grid com espaçamento dx e dy
+% xticks(min(x):dx:max(x));  % Linhas verticais a cada dx
+% yticks(min(y):dy:max(y));  % Linhas horizontais a cada dy
+% grid on;
 
-psi = 500*ones(size(X));
+psi = 0*ones(size(X));
 psi(isSolido) = 0;
 psi = cond_contorno(X,Y,dx,dy,H,V,psi);
+psi_aux=psi;
 
 erro = 1;
 while max(abs(erro(:))) > toleracia
-    psi_aux = psi;
+    mesh(X,Y,psi)
+    psi_aux=psi;
     for i=2:size(X,2)-1
         for j=2:size(Y,1)-1
             if ~isSolido(j,i)
                 if isSolido(j,i-1) || isSolido(j,i+1) || isSolido(j-1,i)
                     if isSolido(j, i+1) && ~isSolido(j-1, i)
                         %Quadrante 1
-                        a = (d-X(j,i))/dx;
+                        if Y(j,i) > h
+                            a = abs(((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx);
+                        else
+                            a = abs((d-X(j,i))/dx);
+                        end
                         psi(j,i)=(2/(a*dx^2)+2/dy^2)^(-1)*(2*psi(j,i-1)/(dx^2*(a+1))+(psi(j+1,i)+psi(j-1,i))/dy^2);
-                    
+                   
                     elseif isSolido(j, i+1) && isSolido(j-1, i)
                         %Quadrante 2
-                        a = ((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx;
-                        b = (Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy;
-                        psi(j,i)=(2/(a*dx^2)+2/(b*dy^2))^(-1)*(2*psi(j,i-1)/(dx^2*(a+1))+2*psi(j+1,i)/(dy^2*(b+1))); 
-                    
+                        a = abs(((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx);
+                        b = abs((Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy);
+                        psi(j,i)=(2/(a*dx^2)+2/(b*dy^2))^(-1)*(2*psi(j,i-1)/(dx^2*(a+1))+2*psi(j+1,i)/(dy^2*(b+1)));
+                   
                     elseif isSolido(j, i-1) && isSolido(j-1, i)
                         %Quadrante 3
-                        a = ((d + L/2 - sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx;
-                        b = (Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy;
+                        a = abs(((d + L/2 + sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx);
+                        b = abs((Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy);
                         psi(j,i)=(2/(a*dx^2)+2/(b*dy^2))^(-1)*(2*psi(j,i+1)/(dx^2*(a+1))+2*psi(j+1,i)/(dy^2*(b+1)));
-                        
-                    else %isSolido(j, i-1) && ~isSolido(j-1, i)
+                       
+                    elseif isSolido(j, i-1) && ~isSolido(j-1, i)
                         %Quadrante 4
-                        a = (X(j,i)-(d+L))/dx;
+                        if Y(j,i) > h
+                            a = abs(((d + L/2 + sqrt((L/2)^2 - (Y(j,i) - h)^2))-X(j,i))/dx);
+                        else
+                            a = abs(((d+L)-X(j,i))/dx);
+                        end
                         psi(j,i)=(2/(a*dx^2)+2/dy^2)^(-1)*(2*psi(j,i+1)/(dx^2*(a+1))+(psi(j+1,i)+psi(j-1,i))/dy^2);
-                        
+                       
+                    elseif (~isSolido(j,i-1) && ~isSolido(j,i+1)) && isSolido(j-1,i)
+                        b = abs((Y(j,i)-(sqrt((L/2)^2 - (X(j,i) - (d + L/2))^2) + h))/dy);
+                        psi(j,i) = (2/(dx^2) + 2/(b*dy^2))^(-1)*((psi(j,i+1)+psi(j,i-1))/(dx^2)+2*psi(j+1,i)/(b+1));
+                    else
+                        disp("não devia ter entrado aqui")
                     end
                 else
                     %Pontos interiores
-                    psi(j,i) = (psi(j,i+1)+psi(j,i-1)+psi(j+1,i)+psi(j-1,i))/4;
+%                     psi_termo = (psi(j,i+1)+psi(j,i-1)+psi(j+1,i)+psi(j-1,i))/4;
+%                     psi_aux(j,i) = lambda*psi_termo+(1-lambda)*psi(j,i);
+                    psi(j,i) = (lambda/4)*(psi(j-1,i)+psi(j+1,i)+psi(j,i-1)+psi(j,i+1)) + (1-lambda)*psi_aux(j,i);
                 end    
-            end 
+            end
         end
     end
-    psi(2:end-1,2:end-1)=lambda*psi(2:end-1,2:end-1)+(1-lambda)*psi_aux(2:end-1,2:end-1);
-    psi = cond_contorno(X,Y,dx,dy,H,V,psi);
-    erro = abs((psi-psi_aux)./psi);
+%     psi(2:end-1,2:end-1)=lambda*psi(2:end-1,2:end-1)+(1-lambda)*psi_aux(2:end-1,2:end-1);
+%     psi = cond_contorno(X,Y,dx,dy,H,V,psi);
+    erro = abs((psi - psi_aux) ./ max(abs(psi), 1e-10));
     display(max(erro(:)));
-    contourf(X, Y, psi, 20, 'LineColor', 'none');
-    colorbar;
-    axis equal;
-    xlabel('x'); ylabel('y');
-    title('Função de Corrente Genérica (\psi)');
 end
 
+figure;
+contourf(X, Y, psi, 20, 'LineColor', 'none');
+colorbar;
+axis equal;
+xlabel('x'); ylabel('y');
+title('Função de Corrente Genérica (\psi)');
+hold on;
+scatter(X(isSolido), Y(isSolido), 10, 'k', 'filled'); % Destacar o sólido
 
+% Calcular velocidades (apenas para pontos fora do sólido)
+u = zeros(size(psi));
+v = zeros(size(psi));
+u(~isSolido) = gradient(psi(~isSolido), dy);  % u = ∂ψ/∂y
+v(~isSolido) = -gradient(psi(~isSolido), dx); % v = -∂ψ/∂x
 
-% % Exibir a matriz psi - Item A
-% figure;
-% contourf(X, Y, psi, 20, 'LineColor', 'none');
-% colorbar;
-% axis equal;
-% xlabel('x'); ylabel('y');
-% title('Função de Corrente Genérica (\psi)');
-% hold on;
-% scatter(X(isSolido), Y(isSolido), 2, 'r', 'filled'); % Destacar o sólido
-% 
-% % Calcular velocidades (apenas para pontos fora do sólido)
-% u = zeros(size(psi));
-% v = zeros(size(psi));
-% u(~isSolido) = gradient(psi(~isSolido), dy);  % u = ∂ψ/∂y
-% v(~isSolido) = -gradient(psi(~isSolido), dx); % v = -∂ψ/∂x
-% 
-% % Plotar campo de velocidade (setas pretas) - ItemB
-% figure;
-% quiver(X(mask), Y(mask), u(mask), v(mask), ...
-%     'AutoScaleFactor', 1.5, ...
-%     'Color', 'k', ...          % Define cor preta
-%     'LineWidth', 1.2);         % Espessura das setas
-% axis equal;
-% grid on;                       % Adiciona grade
-% xlabel('x (m)'); 
-% ylabel('y (m)');
-% title('Campo de Velocidade (Genérico)');
-% 
-% % Ajustar limites dos eixos (se necessário)
-% xlim([min(x), max(x)]);
-% ylim([min(y), max(y)]);
-% 
-% %Pressão item C
-% 
-% % Calcular a pressão (equação corrigida)
-% gamma_term = (gamma_ar - 1) / gamma_ar;
-% DeltaP = rho * gamma_term * ( (V^2)/2 - (u.^2 + v.^2)/2 );
-% 
-% % Mascarar pontos dentro do sólido (isSolido)
-% DeltaP(isSolido) = NaN; % Ignorar pressão no interior
-% 
-% % Plotar pressão
-% figure;
-% contourf(X, Y, DeltaP, 50, 'LineColor', 'none');
-% colorbar;
-% colormap('jet');
-% hold on;
-% scatter(X(isSolido), Y(isSolido), 2, 'k', 'filled'); % Destacar o sólido
-% axis equal;
-% xlabel('x (m)');
-% ylabel('y (m)');
-% title('Distribuição de Pressão Relativa (p - p_{atm}) [Pa]');
-% 
-% % Item D
+% Plotar campo de velocidade (setas pretas) - ItemB
+figure;
+quiver(X(mask), Y(mask), u(mask), v(mask), ...
+    'AutoScaleFactor', 1.5, ...
+    'Color', 'k', ...          % Define cor preta
+    'LineWidth', 1.2);         % Espessura das setas
+axis equal;
+grid on;                       % Adiciona grade
+xlabel('x (m)');
+ylabel('y (m)');
+title('Campo de Velocidade (Genérico)');
 
 function psi = cond_contorno(X,Y,dx,dy,H,V,psi)
-    
+   
     %Margem inferior
     psi(1,:)=0;
-    
-    %Margem esquerda
-    for j = 2:size(Y,1)-1
+   
+    %Margem esquerda e direita
+    for j = 2:size(Y,1)
         if Y(j,1)<=0.05*H
-            psi(j,1)=psi(j,2)+V*dx^2/(0.1*H);
+            psi(j,1)=V*(Y(j,1))^2/(0.1*H);
+            psi(j,end)=V*(Y(j,end))^2/(0.1*H);
         else
-            psi(j,1)=psi(j,2);
+            psi(j,1)=V*Y(j,1)+V*(1-0.05*H);
+            psi(j,end)=V*Y(j,end)+V*(1-0.05*H);
         end
     end
-    
-    %Margem direita
-    for j = 2:size(Y,1)-1
-        if Y(j,end)<=0.05*H
-            psi(j,end)=psi(j,end-1)+V*dx^2/(0.1*H);
-        else
-            psi(j,end)=psi(j,end-1);
-        end
-    end
-    
+   
+    psi(end,:) = psi(end,1);
+
+
+
+   
+%     %Margem direita
+%     for j = 2:size(Y,1)
+%         if Y(j,end)<=0.05*H
+%             psi(j,end)=psi(j,end-1)+(V*dx^2)/(0.1*H);
+%         else
+%             psi(j,end)=psi(j,end-1);
+%         end
+%     end
+   
     %Margem superior
-    for i = 1:size(X,2)
-        psi(end,i) = psi(end-1,i)+dy*V;
-    end
+%     for i = 2:size(X,2)-1
+%         psi(end,i) = psi(end-1,i)+dy*V;
+%     end
 
 end
-
-
-
